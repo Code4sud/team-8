7a5aa18ce1a4b9e3841634cbab9d39d52c0d1f32
thomas-spinec <thomas.spinec@laplateforme.io>
2024-10-03 11:55:46 +0200

    Fix .env file from being tracked in version control

---
+++ back/routes/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+module.exports = async function (fastify, opts) {
+  fastify.get("/", async function (request, reply) {
+    return "this is an example";
+  });
+
+  fastify.get("/test", async function (request, reply) {
+    return { test: true };
+  });
+};
