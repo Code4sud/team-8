666339bc60f82c64c58f41d525bee6dc65b42598
Fatima El mouhine <91740491+fatima-elmouhine@users.noreply.github.com>
2024-10-04 13:57:25 +0200

    Update Logo.tsx

--- front/src/components/Sidebar/Logo.tsx
+++ front/src/components/Sidebar/Logo.tsx
@@ -3,7 +3,7 @@ export const Logo = () => {
     <div className="flex items-center justify-start mb-3 rounded hover:bg-Iqanto-orange-50">
       <div className="px-1 -space-y-1">
         <img
-          src="public/logoGreen.webp"
+          src="logoGreen.webp"
           alt="logo greenair"
           className="w-54 "
         />
