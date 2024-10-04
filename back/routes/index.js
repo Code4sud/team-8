634aa404143457b4fd125a4d8e8b1b89c59d244b
athomaj <julien@athomas.io>
2024-10-04 09:27:17 +0200

    removing / route

--- back/routes/index.js
+++ back/routes/index.js
@@ -7,14 +7,6 @@ const {
 } = require("../request");
 
 module.exports = async function (fastify, opts) {
-  fastify.get("/", async function (request, reply) {
-    return "this is an example";
-  });
-
-  fastify.get("/test", async function (request, reply) {
-    return { test: true };
-  });
-
   fastify.get("/data/:tableName", async function (request, reply) {
     const { tableName } = request.params;
     const data = await getDataFromTable(tableName);
